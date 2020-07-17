8fa95504d845d36ec0f8d67ceeb951e9e7b5e265
DevJac <dbuttonz@gmail.com>
2020-07-16 22:53:12 -0600

    Use vec3 and write_color in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "vec3.hpp"
 #include <iostream>
 
+void write_color(std::ostream &out, const vec3 &pixel_color) {
+  out << static_cast<int>(pixel_color.x() * 255) << ' '
+      << static_cast<int>(pixel_color.y() * 255) << ' '
+      << static_cast<int>(pixel_color.z() * 255) << '\n';
+}
+
 int main() {
   const int image_width = 256;
   const int image_height = 256;
@@ -10,15 +16,9 @@ int main() {
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "Scanlines remaining: " << j + 1 << '\n' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0.25;
-
-      int ir = static_cast<int>(255 * r);
-      int ig = static_cast<int>(255 * g);
-      int ib = static_cast<int>(255 * b);
-
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      const vec3 pixel_color{float(i) / (image_width - 1),
+                             float(j) / (image_height - 1), 0.25};
+      write_color(std::cout, pixel_color);
     }
   }
   std::cerr << "Done.\n";
